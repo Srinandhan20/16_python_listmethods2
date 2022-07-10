dac616b76dce6f500771844d4d2386ba5f8f4977
Srinandhan20 <107259556+Srinandhan20@users.noreply.github.com>
2022-07-10 16:00:25 +0530

    Add files via upload

---
+++ listandmethods2.py
@@ -0,0 +1,27 @@
+# n=[1,2,3,4,5,7,11,13]
+# print(n)
+# n.insert(1,'hello')
+# print(n)
+# n.insert(2,5)
+# print(n)
+# n.insert(-1,12)
+# print(n)
+
+# n=[10,20,30,40]
+# print(n.pop())
+# n.pop()
+# print(n)
+# n.pop(1)
+# print(n)
+
+n=[1,2,3,4,5]
+n.remove(2)
+
+b=[1,2,3,4,5]
+print(n[::-1])
+n.reverse()
+print(n)
+
+n=[1,5,4,3,2]
+n.sort()
+print(n)
